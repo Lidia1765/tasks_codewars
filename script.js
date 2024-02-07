db4ed6af715f345f85048bebf03b44e34016408f
Lidia1765 <Lkoles2003@mail.ru>
2024-02-07 17:04:17 +0100

    list-filtering

---
+++ script.js
@@ -0,0 +1,12 @@
+function filter_list(l) {
+    let arr = [];
+    for (let i = 0; i < l.length; i++) {
+        if (typeof l[i] === 'number') {
+            arr.push(l[i])
+        }
+    }
+    return arr
+}
+console.log(filter_list([1, 2, 'a', 'b']))//[1,2]
+console.log(filter_list([1, 'a', 'b', 0, 15]))//[1,0,15]
+console.log(filter_list([1, 2, 'aasf', '1', '123', 123]))//[1,2,123]
